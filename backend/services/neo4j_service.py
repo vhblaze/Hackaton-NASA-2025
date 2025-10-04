13318808489ca7000390a5559ba87c2af42700d6
treles <thalesgabriel536@gmail.com>
2025-10-04 11:40:43 -0300

    rotas,neo4jl

---
+++ backend/services/neo4j_service.py
@@ -0,0 +1,14 @@
+from neo4j import GraphDatabase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USER = os.getenv("NEO4J_USER")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
+
+def get_session():
+    return driver.session()
